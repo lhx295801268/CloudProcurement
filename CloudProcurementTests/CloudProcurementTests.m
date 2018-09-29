41e0e6e92d92100ad7bd727fbc82bf3d3af4d284
maeing <maeing@192.168.3.41>
2018-09-29 11:13:06 +0800

    Initial Commit

---
+++ CloudProcurementTests/CloudProcurementTests.m
@@ -0,0 +1,37 @@
+//
+//  CloudProcurementTests.m
+//  CloudProcurementTests
+//
+//  Created by TsunamiLi on 2018/9/29.
+//  Copyright © 2018年 TsunamiLi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CloudProcurementTests : XCTestCase
+
+@end
+
+@implementation CloudProcurementTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
